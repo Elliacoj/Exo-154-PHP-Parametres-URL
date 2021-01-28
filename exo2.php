55ce442e66e33dddf4bcddb2bfe1ff169e9bc2b0
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-28 11:08:47 +0100

    Modif

--- exo2.php
+++ exo2.php
@@ -3,8 +3,7 @@
 if(!isset($_GET['iteration'])) {
     header('location: index.php?error=1');
 }
-else {
-    for($x = 0; $x < $_GET['iteration']; $x++) {
-        echo "Hello world <br>";
-    }
+
+for($x = 0; $x < $_GET['iteration']; $x++) {
+    echo "Hello world <br>";
 }
